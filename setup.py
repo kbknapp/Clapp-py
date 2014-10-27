1ccb94c1940cd9169b74f68a86835832fcf78629
Kevin K <kbknapp@gmail.com>
2014-10-27 07:43:59 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import re
+import ast
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('clapp/clapp.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+setup(
+    name='clapp',
+    author='Kevin Knapp',
+    author_email='kbknapp@gmail.com',
+    version=version,
+    url='http://github.com/kbknapp/Clapp-py',
+    packages=['clapp'],
+    description='A small package for easily creating'
+                ' command line applications',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
